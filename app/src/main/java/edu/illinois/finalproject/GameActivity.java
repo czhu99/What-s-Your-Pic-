4ee24f0ee7da54ed3887dd2ccfea90750a616e4d
czhu99 <cthez72@gmail.com>
2017-12-06 00:21:30 -0600

    Linked play game button to activity_game from main activity

--- app/src/main/java/edu/illinois/finalproject/GameActivity.java
+++ app/src/main/java/edu/illinois/finalproject/GameActivity.java
@@ -1,5 +1,6 @@
 package edu.illinois.finalproject;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -8,4 +9,9 @@ import android.support.v7.app.AppCompatActivity;
 
 public class GameActivity extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
 }
---
+++ app/src/main/java/edu/illinois/finalproject/VerifyPicActivity.java
@@ -0,0 +1,11 @@
+package edu.illinois.finalproject;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Chris Zhu on 12/5/2017.
+ */
+
+public class VerifyPicActivity extends AppCompatActivity {
+
+}
