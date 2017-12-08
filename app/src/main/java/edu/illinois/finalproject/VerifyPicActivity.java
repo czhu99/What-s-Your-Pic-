19b8cef45736c9bcdeb2e10c5328d63f3d1456b7
czhu99 <cthez72@gmail.com>
2017-12-08 15:06:56 -0600

    Uploads image to Firebase after confirmation page then kills the activity

--- app/src/main/java/edu/illinois/finalproject/VerifyPicActivity.java
+++ app/src/main/java/edu/illinois/finalproject/VerifyPicActivity.java
@@ -1,25 +1,119 @@
 package edu.illinois.finalproject;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
 
 /**
  * Created by Chris Zhu on 12/5/2017.
  */
 
 public class VerifyPicActivity extends AppCompatActivity {
+    private int photoNumber;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verifypic);
 
         Intent intent = getIntent();
-        Bitmap picture = intent.getParcelableExtra("Picture");
-
+        final Bitmap picture = intent.getParcelableExtra("Picture");
         ImageView photoDisplayView = (ImageView) findViewById(R.id.uploadImageView);
         photoDisplayView.setImageBitmap(picture);
+
+        final Button uploadButton = (Button) findViewById(R.id.uploadButton);
+        uploadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uploadFile(picture);
+                uploadCaptionData();
+                showToast("Image uploaded.");
+                finish();
+            }
+        });
+    }
+
+    /**
+     * Uploads a photo into the database
+     *
+     * @param bitmap The photo to be uploaded
+     */
+    private void uploadFile(Bitmap bitmap) {
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference storageRef =
+                storage.getReferenceFromUrl("gs://final-project-17c20.appspot.com/images");
+        StorageReference imageReference = storageRef.child("image" + photoNumber + ".jpg");
+        photoNumber++;
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 20, byteArrayOutputStream);
+        byte[] data = byteArrayOutputStream.toByteArray();
+        UploadTask uploadTask = imageReference.putBytes(data);
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle unsuccessful uploads
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+            }
+        });
+    }
+
+    /**
+     * Allows for assignment of a caption for a certain photo into the database.
+     * Uses the photo id to pair a caption to a photo.
+     */
+    private void uploadCaptionData() {
+        final EditText captionEditText = (EditText) findViewById(R.id.captionEditText);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference myRef = database.getReference("image" + photoNumber);
+
+        final String message = captionEditText.getText().toString();
+        myRef.setValue(message);
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void showToast(String message) {
+        //outline for toast code segment from
+        //https://developer.android.com/guide/topics/ui/notifiers/toasts.html
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, message, duration);
+        toast.show();
     }
 }
